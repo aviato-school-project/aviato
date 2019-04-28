ea7eb18ce2cdcbb43478da1e5d9b9cc0bc266049
hikeislife <hikeislife@users.noreply.github.com>
2019-04-28 23:10:13 +0200

    error handling

---
+++ Aviato/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aviato.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            return View();
+        }
+
+        public ActionResult ISE()
+        {
+            return View();
+        }
+    }
+}
